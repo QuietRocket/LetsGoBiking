d0468f044a4d50926922acdb6f9f622cf82c5f09
QuietRocket <nik@quietrocket.com>
2023-11-15 20:13:29 +0100

    Added client files

---
+++ Client/src/main/java/com/letsgobiking/App.java
@@ -0,0 +1,13 @@
+package com.letsgobiking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
