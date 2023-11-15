60fedccb53b375d90e9f6307ae334fe922622536
QuietRocket <nik@quietrocket.com>
2023-11-15 22:57:54 +0100

    Docker compose. Health checks for router.

--- Router/Program.cs
+++ Router/Program.cs
@@ -1,5 +1,7 @@
 ﻿var builder = WebApplication.CreateBuilder();
 
+builder.Services.AddHealthChecks();
+
 builder.Services.AddServiceModelServices();
 builder.Services.AddServiceModelMetadata();
 builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
@@ -9,9 +11,12 @@ var app = builder.Build();
 app.UseServiceModel(serviceBuilder =>
 {
     serviceBuilder.AddService<Service>();
-    serviceBuilder.AddServiceEndpoint<Service, IService>(new BasicHttpBinding(BasicHttpSecurityMode.Transport), "/Service.svc");
+    serviceBuilder.AddServiceEndpoint<Service, IService>(new BasicHttpBinding(), "/Service.svc");
     var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
-    serviceMetadataBehavior.HttpsGetEnabled = true;
+
+    serviceMetadataBehavior.HttpGetEnabled = serviceMetadataBehavior.HttpsGetEnabled = true;
 });
+
+app.MapHealthChecks("/health");
 
 app.Run();
