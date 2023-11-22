cd3143cc6da369d8fa09c71f118cc39f777f618e
QuietRocket <nik@quietrocket.com>
2023-11-23 00:30:45 +0100

    Working client which can communicate with Router

---
+++ Client/src/main/java/com/letsgobiking/Main.java
@@ -0,0 +1,20 @@
+package com.letsgobiking;
+
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.tempuri.IService;
+
+public class Main {
+    public static void main(String[] args) {
+        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+        factory.setServiceClass(IService.class);
+        factory.setAddress("http://localhost:5229/Service.svc");
+
+        IService port = (IService) factory.create();
+
+        // Call the getData method
+        String response = port.getData(10);
+
+        // Print the response
+        System.out.println(response);
+    }
+}
